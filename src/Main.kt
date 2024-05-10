14b3882e2bfcfc277a76b0aa1e690c0dd5b4be58
samwanyua <samexample8@gmail.com>
2024-05-10 21:38:49 +0300

    create variables

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,36 @@
+// Global variables
+val role: String = "Mobile Developer";
+
+var greeting: String = "Hello"
+
 fun main() {
-    println("Hello World!")
+    //    Variables
+//    mutable variables - can be reassigned
+//    Read only variables - can't be assigned
+    println("Hello, just started Kotlin!")
+    println(greeting)
+
+    greeting = "Hi"
+
+    println(greeting)
+
+//    nullable string
+    val parentName: String? = null
+
+    println(parentName)
+
+
+
+
+    val userName: String = "johnSam"; // can't be reassigned
+
+    var profileStatus: String = "Qualified"; // can be reassigned
+    profileStatus = "Excellent";
+
+    println(profileStatus);
+    println(role)
+
+
+
+
 }
