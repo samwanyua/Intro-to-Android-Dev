1ae8245e6edb040b1fddbbb329abd2677563d796
samwanyua <samexample8@gmail.com>
2024-05-11 17:12:52 +0300

    Implement anonymous classes

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,15 @@
 fun main() {
 //    val dog = Animal("dog")
-    val dog = Dog()
-    dog.bark()
+//    val dog = Dog()
+//    dog.bark()
 
-    val cat = Cat()
-    cat.sound()
+//    val cat = Cat()
+//    cat.sound()
+//    anonymous classes
+    val bear = object : Animal("PolarBear"){
+        override fun makeSound() {
+            println("Roooaaaaaaar!!")
+        }
+    }
 
 }
