40f529f310bef6d44eaf14e8d3c874529c7844f5
samwanyua <samexample8@gmail.com>
2024-05-11 15:23:59 +0300

    Use data types and variables

--- src/Main.kt
+++ src/Main.kt
@@ -1,69 +1,18 @@
-// Global variables
-val role: String = "Mobile Developer";
-
-var greeting: String = "Hello user"
-
 fun main() {
-    //    Variables
-//    mutable variables - can be reassigned
-//    Read only variables - can't be assigned
-    println("Hello, just started Kotlin!")
-    println(greeting)
+    println("I am back to Kotlin again!")
 
-    greeting = "Hi"
+//    Variables
+    var x: Int = 34; // 32 bit (2^32)
+    println("The value of x is $x")
+    x = 56;
+    println("The value of x is $x")
 
-    println(greeting)
-
-//    nullable string
-    val parentName: String? = null
-
-    println(parentName)
-
-
-
-
-    val userName: String = "johnSam"; // can't be reassigned
-
-    var profileStatus: String = "Qualified"; // can be reassigned
-    profileStatus = "Excellent";
-
-    println(profileStatus);
-    println(role)
-
-//    //    Variables
-//    mutable variables - can be reassigned
-//    Read only variables - can't be assigned
-//
 //    Types
-    /*
-    You can still do this:
-        val name = "George";
-     */
-// control flow
-    var feeStatus: String = "Ken"
-
-    if (feeStatus != null){
-        println(feeStatus +  " your fees is paid")
-    }
-    else{
-        println("You haven't paid")
-    }
-
-//    when statement
-    when(feeStatus){
-        null -> println("You are okay")
-        else -> println("Not paid")
-    }
-
-    val greetingToPrint = if(greeting !=null) greeting else "Jambo"
-    println(greetingToPrint)
-
-    val printGreeting = when(greeting){
-        null -> "Hi User"
-        else -> greeting
-    }
-
-    println(printGreeting)
+    val bigNumber = 324L // this is a long - 64 bit
+    val doubleNumber = 32.234 // double - more precision
+    val floatNumber = 23.343f // float
+    val trueFalse = false // boolean
+    val singleCharacter = 'a' //character
 
 
 
